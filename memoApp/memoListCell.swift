c3e12b62c75c3b64c08fb6cf3ae195654851336c
tatsuyaohyama <t.ohyama0808@gmail.com>
2019-03-24 22:20:37 +0900

    Add memoListCell files

---
+++ memoApp/memoListCell.swift
@@ -0,0 +1,24 @@
+//
+//  memoListCell.swift
+//  memoApp
+//
+//  Created by tatsuya ohyama on 2019/03/24.
+//  Copyright © 2019 tatsuya.ohyama. All rights reserved.
+//
+
+import UIKit
+
+class memoListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
