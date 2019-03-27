7bdd5abd9859795c99c1925893d61dfb7c9e98f1
tatsuyaohyama <t.ohyama0808@gmail.com>
2019-03-27 11:57:58 +0900

    Add get memoData logic

--- memoApp/MemoListViewController.swift
+++ memoApp/MemoListViewController.swift
@@ -7,30 +7,49 @@
 //
 
 import UIKit
+import RealmSwift
 
 class MemoListViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
+    
+    var memoList: Results<MyData>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let nib = UINib(nibName: "MemoListCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "MemoListCell")
+        
+        getMemoData()
+    }
+    
+    func getMemoData() {
+        do{
+            let realm = try Realm()
+            memoList = realm.objects(MyData.self)
+            self.tableView.reloadData()
+        }catch{
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tableView.reloadData()
     }
 }
 
 extension MemoListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return memoList.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell =  tableView.dequeueReusableCell(withIdentifier: "MemoListCell", for: indexPath) as? MemoListCell else {
             fatalError("The dequeued cell is not instance of MemoListCell.")
         }
-        cell.memoTitleLabel.text = "test\(indexPath.row)"
-        
+        cell.memoTitleLabel.text = memoList[indexPath.row].memoData
+
         return cell
     }
     
