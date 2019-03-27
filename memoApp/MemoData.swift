d67af7911eb9df222954392760a9f272a2814724
tatsuyaohyama <t.ohyama0808@gmail.com>
2019-03-27 11:49:21 +0900

    Add realm model object file

---
+++ memoApp/MemoData.swift
@@ -0,0 +1,14 @@
+//
+//  MemoData.swift
+//  memoApp
+//
+//  Created by tatsuya ohyama on 2019/03/27.
+//  Copyright © 2019 tatsuya.ohyama. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class MyData: Object {
+    @objc dynamic var memoData: String = ""
+}
